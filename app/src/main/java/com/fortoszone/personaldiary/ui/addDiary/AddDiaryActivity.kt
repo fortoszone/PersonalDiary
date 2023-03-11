e19f4ed0d20a69e85ce840a71e668d9b6355e4fd
Devwanto Dariel Nugroho <fortoszone@gmail.com>
2023-03-11 15:15:20 +0700

    more slicing

---
+++ app/src/main/java/com/fortoszone/personaldiary/ui/addDiary/AddDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.fortoszone.personaldiary.ui.addDiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_diary)
+    }
+}
