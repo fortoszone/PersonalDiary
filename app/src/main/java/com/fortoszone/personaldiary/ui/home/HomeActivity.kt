55fface8b4567dcbc2e3c8041fa6e32a142db339
Devwanto Dariel Nugroho <fortoszone@gmail.com>
2023-03-11 21:28:37 +0700

    add homepage

--- app/src/main/java/com/fortoszone/personaldiary/ui/home/HomeActivity.kt
+++ app/src/main/java/com/fortoszone/personaldiary/ui/home/HomeActivity.kt
@@ -1,12 +1,48 @@
 package com.fortoszone.personaldiary.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fortoszone.personaldiary.R
+import com.fortoszone.personaldiary.adapter.HomeAdapter
+import com.fortoszone.personaldiary.databinding.ActivityHomeBinding
+import com.fortoszone.personaldiary.databinding.ActivitySplashBinding
+import com.fortoszone.personaldiary.model.local.Diary
+import com.fortoszone.personaldiary.ui.addDiary.AddDiaryActivity
+import com.fortoszone.personaldiary.ui.archive.ArchiveActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var rvDiaries: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        homeViewModel = HomeViewModel()
+
+        rvDiaries = binding.rvDiaries
+        rvDiaries.setHasFixedSize(true)
+        loadRecyclerView()
+
+        binding.btnArchive.setOnClickListener {
+            val intent = Intent(this@HomeActivity, ArchiveActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnAddNewDiary.setOnClickListener {
+            val intent = Intent(this@HomeActivity, AddDiaryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loadRecyclerView() {
+        rvDiaries.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        homeViewModel.loadDiaries(this) {diaries: List<Diary> ->
+            rvDiaries.adapter = HomeAdapter(diaries)}
     }
 }
--- app/src/main/java/com/fortoszone/personaldiary/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/fortoszone/personaldiary/ui/home/HomeViewModel.kt
@@ -1,6 +1,33 @@
 package com.fortoszone.personaldiary.ui.home
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import com.fortoszone.personaldiary.model.local.Diary
+import com.fortoszone.personaldiary.model.remote.response.DiaryResponse
+import com.fortoszone.personaldiary.model.remote.retrofit.DiaryService
+import com.fortoszone.personaldiary.model.remote.retrofit.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
+    fun loadDiaries(context: Context, callback: (List<Diary>) -> Unit) {
+        val apiService = RetrofitClient.getInstance().create(DiaryService::class.java)
+        apiService.getDiaryList()
+            .enqueue(object : Callback<DiaryResponse> {
+                override fun onResponse(
+                    call: Call<DiaryResponse>,
+                    response: Response<DiaryResponse>
+                ) {
+                    return callback(response.body()!!.diaries)
+                }
+
+                override fun onFailure(call: Call<DiaryResponse>, t: Throwable) {
+                    Toast.makeText(context, t.message, Toast.LENGTH_LONG)
+                        .show()
+
+                }
+            })
+    }
 }
